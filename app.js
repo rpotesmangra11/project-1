09579829bbdca74467f81e23b2a5842c394df7d2
Mobious <sammiel.gross@gmail.com>
2018-08-18 13:12:13 -0500

    firebase

---
+++ app.js
@@ -0,0 +1,12 @@
+$(document).ready( ()=> {
+    console.log(ready)
+  var config = {
+    apiKey: "AIzaSyC1Oz8pjHo7EhS4Q6yEdy8e1wzNlMvUu0I",
+    authDomain: "bootcamp-55224.firebaseapp.com",
+    databaseURL: "https://bootcamp-55224.firebaseio.com",
+    projectId: "bootcamp-55224",
+    storageBucket: "bootcamp-55224.appspot.com",
+    messagingSenderId: "90989287982"
+  };
+  firebase.initializeApp(config);
+});
