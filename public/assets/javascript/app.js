d1dd2f956d835a9b1163727d2604852db6a183b9
Ravilicious Def <potesmangrar11@gmail.com>
2018-08-21 21:08:31 -0500

    add wireframe files

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,12 +1,24 @@
 $(document).ready( ()=> {
     console.log("ready")
-  var config = {
-    apiKey: "AIzaSyC1Oz8pjHo7EhS4Q6yEdy8e1wzNlMvUu0I",
-    authDomain: "bootcamp-55224.firebaseapp.com",
-    databaseURL: "https://bootcamp-55224.firebaseio.com",
-    projectId: "bootcamp-55224",
-    storageBucket: "bootcamp-55224.appspot.com",
-    messagingSenderId: "90989287982"
-  };
-  firebase.initializeApp(config);
+    var config = {
+        apiKey: "AIzaSyCe6RdQ713wNfR_pyx9yFVpRp82_YehtOs",
+        authDomain: "project-1-f279d.firebaseapp.com",
+        databaseURL: "https://project-1-f279d.firebaseio.com",
+        projectId: "project-1-f279d",
+        storageBucket: "project-1-f279d.appspot.com",
+        messagingSenderId: "840313509707"
+      };
+      firebase.initializeApp(config);
+});
+
+$("#add-file").on("click", function(event) {
+    event.preventDefault();
+
+    var face = $("#file-input").val();
+    
+    $.ajax({
+        url:'upload.php',
+        type: 'POST',   
+         
+    });
 });
