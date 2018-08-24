e40297fd87b7fa3e43807c59bf4901d6df85ccc2
Kane <kmcgree14@gmail.com>
2018-08-23 20:53:41 -0500

    file upload and face api working

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -11,6 +11,7 @@ $(document).ready(() => {
     firebase.initializeApp(config);
 });
 
+//Image Upload functions
 $(function () {
     $(":file").change(function () {
         if (this.files && this.files[0]) {
@@ -20,8 +21,77 @@ $(function () {
         }
     });
 });
-
 function imageIsLoaded(e) {
-    $('#user1-face').attr('src', e.target.result);
-    console.log(e.target.result)
+    var imgdataURL = e.target.result;
+    $('#user1-face').attr('src', imgdataURL);
+    console.log(imgdataURL);
+    processImage()
+
+    //Face API
+    function processImage() {
+        var subscriptionKey = "06fb82297d134fde9722b84fab719531";
+        var uriBase =
+        "https://westcentralus.api.cognitive.microsoft.com/face/v1.0";
+
+        // Request parameters.
+        var params = {};
+
+        //Make Blob for Ajax call
+        makeblob = function (dataURL) {
+            var BASE64_MARKER = ';base64,';
+            if (dataURL.indexOf(BASE64_MARKER) == -1) {
+                var parts = dataURL.split(',');
+                var contentType = parts[0].split(':')[1];
+                var raw = decodeURIComponent(parts[1]);
+                return new Blob([raw], { type: contentType });
+            }
+            var parts = dataURL.split(BASE64_MARKER);
+            var contentType = parts[0].split(':')[1];
+            var raw = window.atob(parts[1]);
+            var rawLength = raw.length;
+
+            var uInt8Array = new Uint8Array(rawLength);
+
+            for (var i = 0; i < rawLength; ++i) {
+                uInt8Array[i] = raw.charCodeAt(i);
+            }
+
+            return new Blob([uInt8Array], { type: contentType });
+        }
+        console.log(makeblob(imgdataURL));
+
+        // Perform the REST API call.
+        $.ajax({
+            url: uriBase + "?" + $.param(params),
+
+            // Request headers.
+            beforeSend: function (xhrObj) {
+                xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+
+            type: "POST",
+            processData: false,
+            data: makeblob(imgdataURL),
+        })
+
+            .done(function (data) {
+                // Show formatted JSON on webpage.
+                console.log(JSON.stringify(data, null, 2));
+            })
+
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                // Display error message.
+                var errorString = (errorThrown === "") ?
+                    "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+                errorString += (jqXHR.responseText === "") ?
+                    "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
+                        jQuery.parseJSON(jqXHR.responseText).message :
+                        jQuery.parseJSON(jqXHR.responseText).error.message;
+                alert(errorString);
+            });
+    };
 };
+
+
+
